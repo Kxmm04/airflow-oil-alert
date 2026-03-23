47f66fc7b3cbc05acfdd5d3fa2dc8e00e39cb9ae
kxmm04 <Arpiwat0204@gmail.com>
2026-03-24 01:51:33 +0700

    update

--- start.sh
+++ start.sh
@@ -11,5 +11,4 @@ airflow users create \
   --email admin@example.com \
   --password airflow || true
 
-airflow scheduler > /tmp/airflow-scheduler.log 2>&1 &
 exec airflow webserver --hostname 0.0.0.0 --port ${PORT:-8080}
