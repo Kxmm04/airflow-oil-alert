dfb41252797300b24a06925676bcfbef4c36baa9
kxmm04 <Arpiwat0204@gmail.com>
2026-03-24 09:15:24 +0700

    e

--- dags/oil_price_alert.py
+++ dags/oil_price_alert.py
@@ -1,33 +1,9 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-import os
-import requests
 
-def send_line(msg: str):
-    token = os.getenv("LINE_TOKEN")
-    if not token:
-        raise ValueError("ไม่พบ LINE_TOKEN")
-
-    url = "https://api.line.me/v2/bot/message/broadcast"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {token}",
-    }
-    data = {
-        "messages": [
-            {
-                "type": "text",
-                "text": msg,
-            }
-        ]
-    }
-
-    res = requests.post(url, headers=headers, json=data, timeout=30)
-    res.raise_for_status()
-
-def check_price():
-    send_line("✅ ทดสอบ Airflow + LINE สำเร็จ")
+def hello():
+    print("hello airflow")
 
 with DAG(
     dag_id="oil_price_alert",
@@ -35,7 +11,7 @@ with DAG(
     schedule=None,
     catchup=False,
 ) as dag:
-    check_price_task = PythonOperator(
-        task_id="check_price",
-        python_callable=check_price,
+    task = PythonOperator(
+        task_id="hello_task",
+        python_callable=hello
     )
