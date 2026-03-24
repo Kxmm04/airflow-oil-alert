1a965811a3c47ceec3edef02622dac7df21d0188
kxmm04 <Arpiwat0204@gmail.com>
2026-03-24 08:53:56 +0700

    test

--- dags/oil_price_alert.py
+++ dags/oil_price_alert.py
@@ -1,2 +1,17 @@
-def check_price():
-    send_line("✅ ทดสอบ Airflow + LINE สำเร็จ")
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("hello airflow")
+
+with DAG(
+    dag_id="oil_price_alert",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+    task = PythonOperator(
+        task_id="hello_task",
+        python_callable=hello
+    )
